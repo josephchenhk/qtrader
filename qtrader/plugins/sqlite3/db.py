8fa0c6f2fec13d1a471b5ca8ffbd9578e6d0bbfc
josephchenhk <josephchenhk@gmail.com>
2021-04-18 08:54:37 +0800

    [add] update,insert,select functions

--- qtrader/plugins/sqlite3/db.py
+++ qtrader/plugins/sqlite3/db.py
@@ -7,9 +7,10 @@
 
 import sqlite3
 from datetime import datetime
-from typing import Iterable
+from typing import Iterable, List, Dict, Union
 
 import pandas as pd
+import numpy as np
 
 try:
     from qtrader.config import DB
@@ -23,7 +24,6 @@ class DB:
         self.conn = sqlite3.connect(f"{db_path}/qtrader.db")
         self.cursor = self.conn.cursor()
         self.create_balance_table()
-        print()
 
     def close(self):
         self.cursor.close()
@@ -40,28 +40,64 @@ class DB:
         self.cursor.execute(sql, parameters)
         self.commit()
 
-    def delete_table(self, table_name:str):
-        sql = f"DROP TABLE {table_name}"
-        self.execute(sql)
+    def _parse_sql_value(self, value:Union[str,float,int,datetime]):
+        if isinstance(value, str):
+            return f"\"{value}\""
+        elif isinstance(value, float) or isinstance(value, int) or isinstance(value, np.int64):
+            return f"{value}"
+        elif isinstance(value, datetime):
+            return f"\"{value.strftime('%Y-%m-%d %H:%M:%S')}\""
+        else:
+            raise ValueError(f"Data format is not support! type({value})={type(value)}")
 
-    def select_records(self, table_name:str, **kwargs):
-        sql = f"SELECT * FROM {table_name} "
+    def _parse_sql_where_condition(self, **kwargs):
+        sql = ""
         if len(kwargs)>0:
             sql += "WHERE "
         for idx, (k,v) in enumerate(kwargs.items()):
             if idx!=0:
                 sql += "AND "
-            if isinstance(v, str):
-                sql += f"{k}=\"{v}\" "
-            elif isinstance(v, float) or isinstance(v, int):
-                sql += f"{k}={v} "
-            elif isinstance(v, datetime):
-                sql += f"{k}=\"{v.strftime('%Y-%m-%d %H:%M:%S')}\" "
+            if k=="condition_str": # 覆盖所有非"="形式的条件
+                sql += f"{v} "
             else:
-                raise ValueError(f"Data format is not support! type({v})={type(v)}")
+                sql += f"{k}={self._parse_sql_value(v)} "
+        return sql
+
+    def delete_table(self, table_name:str):
+        sql = f"DROP TABLE {table_name}"
+        self.execute(sql)
+
+    def select_records(self, table_name:str, columns:List[str]=None, **kwargs):
+        if columns is None:
+            columns = "*"
+        else:
+            columns = ",".join(columns)
+        sql = f"SELECT {columns} FROM {table_name} "
+        sql += self._parse_sql_where_condition(**kwargs)
         self.execute(sql)
         data = self.cursor.fetchall()
         return pd.DataFrame(data, columns=[d[0] for d in self.cursor.description])
+
+    def update_records(self, table_name:str, columns:Dict[str, Union[str,float,int,datetime]], **kwargs):
+        assert len(columns)>0, "At least one column needs to be updated!"
+        sql = f"UPDATE {table_name} "
+        for idx, (k,v) in enumerate(columns.items()):
+            if idx==0:
+                sql += "SET "
+            sql += f"{k}={self._parse_sql_value(v)} "
+            if idx!=len(columns)-1:
+                sql += ","
+        sql += self._parse_sql_where_condition(**kwargs)
+        self.execute(sql)
+
+    def insert_records(self, table_name:str, **kwargs):
+        assert len(kwargs)>0 ,"Must provide columns and values when inserting!"
+        sql = f"INSERT INTO {table_name} ("
+        sql += ",".join(kwargs.keys())
+        sql += ") VALUES ("
+        sql += ",".join([self._parse_sql_value(v) for v in kwargs.values()])
+        sql += ")"
+        self.execute(sql)
 
     def create_balance_table(self):
         sql = (
@@ -85,33 +121,8 @@ class DB:
         )
         self.execute(sql)
 
-    def insert_balance_table(self,
-                             broker_name:str,
-                             broker_environment:str,
-                             broker_account_id:int,
-                             broker_account:str,
-                             strategy_account_id:int,
-                             strategy_account:str,
-                             strategy_version:str,
-                             strategy_version_desc:str,
-                             strategy_status:str,
-                             cash:float,
-                             power:float,
-                             max_power_short:float,
-                             net_cash_power:float,
-                             update_time:datetime,
-                             remark:str
-        ):
-        sql = (
-            "INSERT INTO balance " +
-            "(broker_name, broker_environment,broker_account_id, broker_account, strategy_account_id, strategy_account, " +
-            "strategy_version, strategy_version_desc, strategy_status, cash, power, max_power_short, net_cash_power, update_time, remark) " +
-            f"VALUES (\"{broker_name}\", \"{broker_environment}\", {broker_account_id}, \"{broker_account}\", " +
-            f"{strategy_account_id}, \"{strategy_account}\", \"{strategy_version}\", \"{strategy_version_desc}\", \"{strategy_status}\", " +
-            f"{cash}, {power}, {max_power_short}, {net_cash_power}, \"{update_time.strftime('%Y-%m-%d %H:%M:%S')}\", \"{remark}\")"
-        )
-        self.execute(sql)
-        print()
+
+
 
 
 
@@ -119,28 +130,37 @@ if __name__=="__main__":
     db = DB()
     # db.delete_table("balance")
     # db.create_balance_table()
-    # db.insert_balance_table(
-    #     broker_name = "FUTU",
-    #     broker_environment = "SIMULATE",
-    #     broker_account_id = 1,
-    #     broker_account = "123456",
-    #     strategy_account_id = 1,
-    #     strategy_account = "default",
-    #     strategy_version = "1.0",
-    #     strategy_version_desc = "manual trading",
-    #     strategy_status="active",
-    #     cash = 100000.0,
-    #     power = 99000,
-    #     max_power_short = -1,
-    #     net_cash_power = -1,
-    #     update_time = datetime.now(),
-    #     remark = "N/A"
-    # )
+
+    db.insert_records(
+        table_name="balance",
+        broker_name = "FUTU2",
+        broker_environment = "SIMULATE",
+        broker_account_id = 1,
+        broker_account = "123456",
+        strategy_account_id = 1,
+        strategy_account = "default",
+        strategy_version = "1.0",
+        strategy_version_desc = "manual trading",
+        strategy_status="active",
+        cash = 100000.0,
+        power = 99000,
+        max_power_short = -1,
+        net_cash_power = -1,
+        update_time = datetime.now(),
+        remark = "N/A"
+    )
+
     records = db.select_records(
         table_name="balance",
-        broker_name="FUTU",
+        broker_name="FUTU2",
         broker_environment="SIMULATE",
         broker_account="123456",
         # strategy_account_id=1,
+    )
+
+    db.update_records(
+        table_name="balance",
+        columns={"cash":950000},
+        id=4
     )
     print()
