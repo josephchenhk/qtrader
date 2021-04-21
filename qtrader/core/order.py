4f2a9f6fa994fc715311b98705f693795108281a
josephchenhk <josephchenhk@gmail.com>
2021-04-21 16:44:40 +0800

    [change] rewrite order and deal to dataclass

--- qtrader/core/order.py
+++ qtrader/core/order.py
@@ -4,44 +4,27 @@
 # @Email   : josephchenhk@gmail.com
 # @FileName: order.py
 # @Software: PyCharm
-
+from dataclasses import dataclass
 from datetime import datetime
 
 from qtrader.core.constants import Direction, Offset, OrderType, OrderStatus
 from qtrader.core.security import Stock
 
+
+@dataclass
 class Order:
     """
     订单
     """
-
-    def __init__(self,
-                 security: Stock,
-                 price: float,
-                 quantity: float,
-                 direction: Direction,
-                 offset: Offset,
-                 order_type: OrderType,
-                 create_time: datetime,
-                 updated_time: datetime=None,
-                 filled_avg_price: float=0,
-                 filled_quantity:int=0,
-                 status: OrderStatus=OrderStatus.UNKNOWN
-    ):
-        self.security = security
-        self.price = price
-        self.quantity = quantity
-        self.direction = direction
-        self.offset = offset
-        self.order_type = order_type
-        self.create_time = create_time
-        self.updated_time = updated_time
-        self.filled_avg_price = filled_avg_price
-        self.filled_quantity = filled_quantity
-        self.status = status
-
-    def __str__(self):
-        return f"Order[{self.security}, price={self.price}, quantity={self.quantity}, direction={self.direction}, " + \
-               f"offset={self.offset}, ordertype={self.order_type}, create_time={self.create_time}, updated_time={self.updated_time}, " +\
-               f"filled_avg_price={self.filled_avg_price}, filled_quantity={self.filled_quantity}, status={self.status}]"
-    __repr__ = __str__
+    security: Stock
+    price: float
+    quantity: float
+    direction: Direction
+    offset: Offset
+    order_type: OrderType
+    create_time: datetime
+    updated_time: datetime = None
+    filled_avg_price: float = 0
+    filled_quantity: int = 0
+    status: OrderStatus = OrderStatus.UNKNOWN
+    orderid: str = ""
