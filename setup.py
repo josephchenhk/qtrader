c847adb7828ba1f618bb103781302aa16bf05d6d
josephchenhk <josephchenhk@gmail.com>
2021-09-16 16:09:57 +0800

    [update] example in setup

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     include_package_data=True,
     entry_points={
         'console_scripts':[
-            'example=tests.test_event:run'
+            'example=examples.demo_strategy:run'
         ]
     },
 )
