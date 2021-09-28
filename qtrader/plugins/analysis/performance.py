c936ee5957a59772e7983fda79141e2298cb856e
josephchenhk <josephchenhk@gmail.com>
2021-09-28 12:14:39 +0800

    [fix] no flat trades

--- qtrader/plugins/analysis/performance.py
+++ qtrader/plugins/analysis/performance.py
@@ -12,7 +12,7 @@ import pandas as pd
 
 def convert_time(time_:str):
     """time_ in the format: %H:%M:%S"""
-    hour, min, sec = time_.split(":")
+    hour, min, *sec = time_.split(":")
     if int(min)<30:
         return f"{hour}:00:00"
     else:
@@ -24,7 +24,7 @@ def holding_period(time_:pd.Series):
     end_dt = datetime.strptime(time_["close_datetime"], "%Y-%m-%d %H:%M:%S")
     return (end_dt - begin_dt).total_seconds() / 60.
 
-result_path = "results/2021-09-24 10-49-10.569006/result.csv"
+result_path = "results/2021-09-24 10-49-10.569006-3min/result.csv"
 result = Path(os.getcwd()).parent.parent.parent.joinpath(result_path)
 df = pd.read_csv(result)
 df_action = df[df["action"].notna()]
@@ -44,7 +44,13 @@ for idx in range(df_action.shape[0]):
     # print(idx, idx + 1)
     open_trade = df_action.loc[idx, :]
     close_trade = df_action.loc[idx+1, :]
-    pnl = close_trade["pv_without_fees"] - open_trade["pv_without_fees"]
+
+    # Remove the impact of fees
+    # pnl = close_trade["pv_without_fees"] - open_trade["pv_without_fees"]
+
+    # Keep the impact of fees
+    pnl = close_trade["portfolio_value"] - open_trade["portfolio_value"]
+
     if pnl>1e-8:
         win_trades.append([
             open_trade["datetime"],
@@ -99,6 +105,9 @@ if flat_trades:
 
 
 with pd.ExcelWriter(result.parent.joinpath("stats.xlsx")) as writer:
+    total_trades = []
+    total_holding_periods = []
+    total_holding_periods_index = []
     if win_trades:
         win_trades_df.to_excel(
             writer,
@@ -109,6 +118,12 @@ with pd.ExcelWriter(result.parent.joinpath("stats.xlsx")) as writer:
             writer,
             sheet_name="win_trades_sessions",
         )
+
+        win_trades_holding_period = win_trades_df.agg({"holding_period": ["max", "min", "mean", "std"]})
+        total_trades.append(win_trades_df)
+        total_holding_periods.append(win_trades_holding_period.T)
+        total_holding_periods_index.append("win_trades")
+
     if loss_trades:
         loss_trades_df.to_excel(
             writer,
@@ -119,6 +134,12 @@ with pd.ExcelWriter(result.parent.joinpath("stats.xlsx")) as writer:
             writer,
             sheet_name="loss_trades_sessions",
         )
+
+        loss_trades_holding_period = loss_trades_df.agg({"holding_period": ["max", "min", "mean", "std"]})
+        total_trades.append(loss_trades_df)
+        total_holding_periods.append(loss_trades_holding_period.T)
+        total_holding_periods_index.append("loss_trades")
+
     if flat_trades:
         flat_trades_df.to_excel(
             writer,
@@ -130,16 +151,19 @@ with pd.ExcelWriter(result.parent.joinpath("stats.xlsx")) as writer:
             sheet_name="flat_trades_sessions",
         )
 
-    pd.concat([win_trades_df, loss_trades_df, flat_trades_df]).groupby("open_session").pnl.agg(["sum", "count", "mean"]).to_excel(
+        flat_trades_holding_period = flat_trades_df.agg({"holding_period": ["max", "min", "mean", "std"]})
+        total_trades.append(flat_trades_df)
+        total_holding_periods.append(flat_trades_holding_period.T)
+        total_holding_periods_index.append("flat_trades")
+
+
+    pd.concat(total_trades).groupby("open_session").pnl.agg(["sum", "count", "mean"]).to_excel(
         writer,
         sheet_name="trades_sessions",
     )
 
-    win_trades_holding_period = win_trades_df.agg({"holding_period": ["max", "min", "mean", "std"]})
-    loss_trades_holding_period = loss_trades_df.agg({"holding_period": ["max", "min", "mean", "std"]})
-    flat_trades_holding_period = flat_trades_df.agg({"holding_period": ["max", "min", "mean", "std"]})
-    holding_period_df = pd.concat([win_trades_holding_period.T, loss_trades_holding_period.T, flat_trades_holding_period.T])
-    holding_period_df.index = ["win_trades", "loss_trades", "flat_trades"]
+    holding_period_df = pd.concat(total_holding_periods)
+    holding_period_df.index = total_holding_periods_index
     holding_period_df.to_excel(
         writer,
         sheet_name="holding_time",
