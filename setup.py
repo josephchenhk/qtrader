14848913985ec6557b629ba28a247edca67ebb32
josephchenhk <josephchenhk@gmail.com>
2021-10-15 18:05:55 +0800

    [fix] specified package explicitly

--- setup.py
+++ setup.py
@@ -17,8 +17,8 @@ setup(
     install_requires=['sqlalchemy', 'pandas', 'numpy', 'pytz', 'clickhouse-driver'], # 这里面填写项目用到的第三方依赖
     author='josephchen',
     author_email='josephchenhk@gmail.com',
-    packages=find_packages(), # 项目内所有自己编写的库
     include_package_data=True,
+    packages=find_packages(), # 项目内所有自己编写的库
     platforms='any',
     url='', # 项目链接,
     include_package_data=True,
