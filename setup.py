d09b209fc46ec3e5653ebbbd2dec8c99a475ab58
josephchenhk <josephchenhk@gmail.com>
2021-10-15 17:40:41 +0800

    [fix] include pyd files

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
     author='josephchen',
     author_email='josephchenhk@gmail.com',
     packages=find_packages(), # 项目内所有自己编写的库
+    include_package_data=True,
     platforms='any',
     url='', # 项目链接,
     include_package_data=True,
