35b686ed56df0a9fb399bd5d4131bed28e98e419
josephchenhk <josephchenhk@gmail.com>
2021-12-30 15:05:45 +0800

    [chg] remove original main and add main_demo

---
+++ main_demo.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+# @Time    : 17/3/2021 3:59 PM
+# @Author  : Joseph Chen
+# @Email   : josephchenhk@gmail.com
+# @FileName: main_demo.py
+
+"""
+Copyright (C) 2020 Joseph Chen - All Rights Reserved
+You may use, distribute and modify this code under the
+terms of the JXW license, which unfortunately won't be
+written for another century.
+
+You should have received a copy of the JXW license with
+this file. If not, please write to: josephchenhk@gmail.com
+"""
+
+########################################################################################################################
+#                                                                                                                      #
+#                                                 Demo strategy                                                        #
+########################################################################################################################
+
+from datetime import datetime
+from datetime import time as Time
+
+from qtrader.core.constants import TradeMode, Exchange
+from qtrader.core.event_engine import BarEventEngineRecorder, BarEventEngine
+from qtrader.core.security import Futures
+from qtrader.core.engine import Engine
+from qtrader.gateways import BacktestGateway
+
+from examples.demo_strategy import DemoStrategy
+
+if __name__=="__main__":
+    stock_list = [
+        Futures(code="HK.01157", lot_size=100, security_name="中联重科", exchange=Exchange.SEHK),
+    ]
+
+    gateway_name = "Backtest"
+    init_capital = 100000
+    gateway = BacktestGateway(
+        securities=stock_list,
+        start=datetime(2021, 3, 15, 9, 30, 0, 0),
+        end=datetime(2021, 3, 17, 16, 0, 0, 0),
+        gateway_name=gateway_name,
+    )
+    gateway.SHORT_INTEREST_RATE = 0.0
+    gateway.set_trade_mode(TradeMode.BACKTEST)
+    # Asia time
+    gateway.TRADING_HOURS_AM = [Time(9, 30, 0), Time(12, 0, 0)]
+    gateway.TRADING_HOURS_PM = [Time(13, 0, 0), Time(16, 0, 0)]
+
+    # 执行引擎
+    engine = Engine(gateways={gateway_name: gateway})
+
+    # 初始化策略
+    strategy_account = "DemoStrategy"
+    strategy_version = "1.0"
+    strategy = DemoStrategy(
+        securities={gateway_name: stock_list},
+        strategy_account=strategy_account,
+        strategy_version=strategy_version,
+        init_strategy_cash={gateway_name: init_capital},
+        engine=engine)
+    strategy.init_strategy()
+
+    # 事件引擎启动
+    recorder = BarEventEngineRecorder()
+    event_engine = BarEventEngine(strategy, recorder)
+    event_engine.run()
+
+    result_path = recorder.save_csv()
+    plugins = engine.get_plugins()
+    if "analysis" in plugins:
+        plot_pnl = plugins["analysis"].plot_pnl
+        plot_pnl(result_path=result_path)
+    engine.log.info("Program shutdown normally.")
