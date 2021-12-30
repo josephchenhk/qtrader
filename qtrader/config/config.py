478b3df65669524564f4e257eddbbce63c90e79a
josephchenhk <josephchenhk@gmail.com>
2021-12-30 15:51:59 +0800

    [chg] update sample config file

--- qtrader/config/config.py
+++ qtrader/config/config.py
@@ -3,7 +3,6 @@
 # @Author  : Joseph Chen
 # @Email   : josephchenhk@gmail.com
 # @FileName: config.py
-# @Software: PyCharm
 
 BACKTEST_GATEWAY = {
     "broker_name": "BACKTEST",
@@ -13,22 +12,51 @@ BACKTEST_GATEWAY = {
     "pwd_unlock": -1,
 }
 
+FUTU_GATEWAY = {
+    "broker_name": "FUTU",
+    "broker_account": "",
+    "host": "",
+    "port": -1,
+    "pwd_unlock": -1,
+}
+
+IB_GATEWAY = {
+    "broker_name": "IB",
+    "broker_account": "",
+    "host": "127.0.0.1",
+    "port": 7497,
+    "clientid": 1,
+    "pwd_unlock": -1,
+}
+
+CQG_GATEWAY = {
+    "broker_name": "CQG",
+    "broker_account": "",
+    "GWAccountID": 1,
+    "password": "",
+    "host": "127.0.0.1",
+    "port": 2823,
+}
+
 GATEWAYS = {
-    "Backtest": BACKTEST_GATEWAY
+    "Backtest": BACKTEST_GATEWAY,
+    "Futu": FUTU_GATEWAY,
+    "Ib": IB_GATEWAY,
+    "Cqg": CQG_GATEWAY
 }
 
 TIME_STEP = 60000  # 设置事件框架监测步长 ms
 
 DATA_PATH = {
-    "k1m": "C:/Users/ABC/data/k_line/K_1M", # "k1m" is must have
+    "kline": "path_to_your_qtrader_folder/examples/data/k_line/K_1M",
 }
 
 DATA_MODEL = {
-    "k1m": "Bar",
+    "kline": "Bar",
 }
 
 DB = {
-    "sqlite3": "examples/data/sqlite3"
+    "sqlite3": "path_to_your_qtrader_folder/examples/data/sqlite3"
 }
 
-ACTIVATED_PLUGINS = ["analysis", "sqlite3"]
+ACTIVATED_PLUGINS = ["analysis"]
