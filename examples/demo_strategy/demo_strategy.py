401f42ff04efa2bf6d822e9dbfc426e6106f7cb7
josephchenhk <josephchenhk@gmail.com>
2022-06-13 16:28:47 +0800

    Update pep8 for demo strat

--- examples/demo_strategy/demo_strategy.py
+++ examples/demo_strategy/demo_strategy.py
@@ -16,7 +16,6 @@ this file. If not, please write to: josephchenhk@gmail.com
 
 from time import sleep
 from typing import Dict, List
-from random import random
 
 import pandas as pd
 from finta import TA
@@ -27,16 +26,18 @@ from qtrader.core.engine import Engine
 from qtrader.core.security import Stock, Security
 from qtrader.core.strategy import BaseStrategy
 
+
 class DemoStrategy(BaseStrategy):
+    """Demo strategy"""
 
     def __init__(self,
-            securities:Dict[str,List[Stock]],
-            strategy_account:str,
-            strategy_version:str,
-            init_strategy_cash:Dict[str,float],
-            engine:Engine,
-            **kwargs
-        ):
+                 securities: Dict[str, List[Stock]],
+                 strategy_account: str,
+                 strategy_version: str,
+                 init_strategy_cash: Dict[str, float],
+                 engine: Engine,
+                 **kwargs
+                 ):
         super().__init__(
             securities=securities,
             strategy_account=strategy_account,
@@ -54,7 +55,7 @@ class DemoStrategy(BaseStrategy):
         # For simulation/live trading, set the waiting time > 0
         self.sleep_time = 0
         for gateway_name in engine.gateways:
-            if engine.gateways[gateway_name].trade_mode!=TradeMode.BACKTEST:
+            if engine.gateways[gateway_name].trade_mode != TradeMode.BACKTEST:
                 self.sleep_time = 5
 
     def init_strategy(self):
@@ -64,9 +65,9 @@ class DemoStrategy(BaseStrategy):
             for security in self.engine.gateways[gateway_name].securities:
                 self.ohlcv[gateway_name][security] = []
 
-    def on_bar(self, cur_data:Dict[str,Dict[Security, Bar]]):
+    def on_bar(self, cur_data: Dict[str, Dict[Security, Bar]]):
 
-        self.engine.log.info("-"*30 + "Enter on_bar" + "-"*30)
+        self.engine.log.info("-" * 30 + "Enter on_bar" + "-" * 30)
         self.engine.log.info(cur_data)
 
         for gateway_name in self.engine.gateways:
@@ -77,20 +78,17 @@ class DemoStrategy(BaseStrategy):
             # check balance
             balance = self.engine.get_balance(gateway_name=gateway_name)
             self.engine.log.info(f"{balance}")
-            broker_balance = self.engine.get_broker_balance(gateway_name=gateway_name)
+            broker_balance = self.engine.get_broker_balance(
+                gateway_name=gateway_name)
             self.engine.log.info(f"{broker_balance}")
 
             # check position
-            positions = self.engine.get_all_positions(gateway_name=gateway_name)
+            positions = self.engine.get_all_positions(
+                gateway_name=gateway_name)
             self.engine.log.info(f"{positions}")
-            broker_positions = self.engine.get_all_broker_positions(gateway_name=gateway_name)
+            broker_positions = self.engine.get_all_broker_positions(
+                gateway_name=gateway_name)
             self.engine.log.info(f"{broker_positions}")
-
-            # check orders
-            all_orders = self.engine.get_all_orders(gateway_name=gateway_name)
-
-            # check deals
-            all_deals = self.engine.get_all_deals(gateway_name=gateway_name)
 
             # send orders
             for security in cur_data[gateway_name]:
@@ -106,13 +104,13 @@ class DemoStrategy(BaseStrategy):
                     self.ohlcv[gateway_name][security].pop(0)
 
                 open_ts = [b.open for b in
-                               self.ohlcv[gateway_name][security]]
+                           self.ohlcv[gateway_name][security]]
                 high_ts = [b.high for b in
-                               self.ohlcv[gateway_name][security]]
+                           self.ohlcv[gateway_name][security]]
                 low_ts = [b.low for b in
-                              self.ohlcv[gateway_name][security]]
+                          self.ohlcv[gateway_name][security]]
                 close_ts = [b.close for b in
-                                self.ohlcv[gateway_name][security]]
+                            self.ohlcv[gateway_name][security]]
 
                 ohlc = pd.DataFrame({
                     "open": open_ts,
@@ -130,7 +128,6 @@ class DemoStrategy(BaseStrategy):
                     continue
 
                 prev_macd = macd.iloc[-2]["MACD"]
-                prev_signal = macd.iloc[-2]["SIGNAL"]
                 cur_macd = macd.iloc[-1]["MACD"]
                 cur_signal = macd.iloc[-1]["SIGNAL"]
                 signal = None
@@ -139,10 +136,10 @@ class DemoStrategy(BaseStrategy):
                 elif prev_macd < cur_signal < cur_macd < 0:
                     signal = "BUY"
 
-                long_position = self.engine.get_position(security=security, direction=Direction.LONG,
-                                                         gateway_name=gateway_name)
-                short_position = self.engine.get_position(security=security, direction=Direction.SHORT,
-                                                         gateway_name=gateway_name)
+                long_position = self.engine.get_position(
+                    security=security, direction=Direction.LONG, gateway_name=gateway_name)
+                short_position = self.engine.get_position(
+                    security=security, direction=Direction.SHORT, gateway_name=gateway_name)
 
                 if short_position and signal == "SELL":
                     continue
@@ -189,24 +186,29 @@ class DemoStrategy(BaseStrategy):
 
                 self.engine.log.info(f"Submit order: \n{order_instruct}")
                 orderid = self.engine.send_order(**order_instruct)
-                if orderid=="":
+                if orderid == "":
                     self.engine.log.info("Fail to submit order")
                     return
 
                 sleep(self.sleep_time)
-                order = self.engine.get_order(orderid=orderid, gateway_name=gateway_name)
-                self.engine.log.info(f"Order ({orderid}) has been sent: {order}")
+                order = self.engine.get_order(
+                    orderid=orderid, gateway_name=gateway_name)
+                self.engine.log.info(
+                    f"Order ({orderid}) has been sent: {order}")
 
-                deals = self.engine.find_deals_with_orderid(orderid, gateway_name=gateway_name)
+                deals = self.engine.find_deals_with_orderid(
+                    orderid, gateway_name=gateway_name)
                 for deal in deals:
                     self.portfolios[gateway_name].update(deal)
 
-                if order.status==OrderStatus.FILLED:
+                if order.status == OrderStatus.FILLED:
                     self.engine.log.info(f"Order ({orderid}) has been filled.")
                 else:
-                    err = self.engine.cancel_order(orderid=orderid, gateway_name=gateway_name)
+                    err = self.engine.cancel_order(
+                        orderid=orderid, gateway_name=gateway_name)
                     if err:
-                        self.engine.log.info(f"Can't cancel order ({orderid}). Error: {err}")
+                        self.engine.log.info(
+                            f"Can't cancel order ({orderid}). Error: {err}")
                     else:
-                        self.engine.log.info(f"Successfully cancel order ({orderid}).")
-
+                        self.engine.log.info(
+                            f"Successfully cancel order ({orderid}).")
