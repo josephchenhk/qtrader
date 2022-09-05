acb8337fccd066bcd95fea8350d0f801d746fc0e
josephchenhk <josephchenhk@gmail.com>
2022-09-05 18:03:25 +0800

    Remove qtrader config pkg

--- __init__.py
+++ __init__.py
@@ -1,18 +1,15 @@
 # -*- coding: utf-8 -*-
-# @Time    : 7/3/2021 9:03 AM
+# @Time    : 5/9/2022 5:54 pm
 # @Author  : Joseph Chen
 # @Email   : josephchenhk@gmail.com
 # @FileName: __init__.py.py
-# @Software: PyCharm
 
 """
 Copyright (C) 2020 Joseph Chen - All Rights Reserved
-You may use, distribute and modify this code under the
+You may use, distribute and modify this code under the 
 terms of the JXW license, which unfortunately won't be
 written for another century.
 
 You should have received a copy of the JXW license with
 this file. If not, please write to: josephchenhk@gmail.com
 """
-
-from .config import *
