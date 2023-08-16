c3396584c9dfa720929e6d773fca27c8aae8564d
josephchenhk <josephchenhk@gmail.com>
2023-08-16 21:20:07 +0800

    add lock to position update

--- qtrader/core/position.py
+++ qtrader/core/position.py
@@ -14,6 +14,7 @@ written for another century.
 You should have received a copy of the JXW license with
 this file. If not, please write to: josephchenhk@gmail.com
 """
+import threading
 
 from dataclasses import dataclass
 from datetime import datetime
@@ -22,6 +23,7 @@ from typing import Dict, List
 from qtrader.core.constants import Direction, Offset
 from qtrader.core.security import Stock
 
+lock = threading.Lock()
 
 @dataclass
 class PositionData:
@@ -43,56 +45,57 @@ class Position:
         self.holdings = holdings
 
     def update(self, position_data: PositionData, offset: Offset):
-        security = position_data.security
-        direction = position_data.direction
-        holding_price = position_data.holding_price
-        quantity = position_data.quantity
-        update_time = position_data.update_time
-        if offset == Offset.OPEN:
-            if security not in self.holdings:
-                self.holdings[security] = {}
-                self.holdings[security][direction] = position_data
-            elif direction not in self.holdings[security]:
-                self.holdings[security][direction] = position_data
-            else:
-                old_position_data = self.holdings[security][direction]
-                new_quantity = old_position_data.quantity + quantity
-                new_total_value = (
-                    old_position_data.holding_price * old_position_data.quantity
-                    + holding_price * quantity
-                )
-                new_holding_price = new_total_value / new_quantity
-                self.holdings[security][direction] = PositionData(
-                    security=security,
-                    direction=direction,
-                    holding_price=new_holding_price,
-                    quantity=new_quantity,
-                    update_time=update_time
-                )
-        elif offset == offset.CLOSE:
-            offset_direction = (
-                Direction.SHORT
-                if direction == Direction.LONG
-                else Direction.LONG)
-            old_position_data = self.holdings[security][offset_direction]
-            new_quantity = old_position_data.quantity - quantity
-            if new_quantity > 0:
-                new_total_value = (
-                    old_position_data.holding_price * old_position_data.quantity
-                    - holding_price * quantity
-                )
-                new_holding_price = new_total_value / new_quantity
-                self.holdings[security][offset_direction] = PositionData(
-                    security=security,
-                    direction=offset_direction,
-                    holding_price=new_holding_price,
-                    quantity=new_quantity,
-                    update_time=update_time
-                )
-            else:
-                self.holdings[security].pop(offset_direction, None)
-        if len(self.holdings[security]) == 0:
-            self.holdings.pop(security, None)
+        with lock:
+            security = position_data.security
+            direction = position_data.direction
+            holding_price = position_data.holding_price
+            quantity = position_data.quantity
+            update_time = position_data.update_time
+            if offset == Offset.OPEN:
+                if security not in self.holdings:
+                    self.holdings[security] = {}
+                    self.holdings[security][direction] = position_data
+                elif direction not in self.holdings[security]:
+                    self.holdings[security][direction] = position_data
+                else:
+                    old_position_data = self.holdings[security][direction]
+                    new_quantity = old_position_data.quantity + quantity
+                    new_total_value = (
+                        old_position_data.holding_price * old_position_data.quantity
+                        + holding_price * quantity
+                    )
+                    new_holding_price = new_total_value / new_quantity
+                    self.holdings[security][direction] = PositionData(
+                        security=security,
+                        direction=direction,
+                        holding_price=new_holding_price,
+                        quantity=new_quantity,
+                        update_time=update_time
+                    )
+            elif offset == offset.CLOSE:
+                offset_direction = (
+                    Direction.SHORT
+                    if direction == Direction.LONG
+                    else Direction.LONG)
+                old_position_data = self.holdings[security][offset_direction]
+                new_quantity = old_position_data.quantity - quantity
+                if new_quantity > 0:
+                    new_total_value = (
+                        old_position_data.holding_price * old_position_data.quantity
+                        - holding_price * quantity
+                    )
+                    new_holding_price = new_total_value / new_quantity
+                    self.holdings[security][offset_direction] = PositionData(
+                        security=security,
+                        direction=offset_direction,
+                        holding_price=new_holding_price,
+                        quantity=new_quantity,
+                        update_time=update_time
+                    )
+                else:
+                    self.holdings[security].pop(offset_direction, None)
+            if len(self.holdings[security]) == 0:
+                self.holdings.pop(security, None)
 
     def get_position(
             self,
